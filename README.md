656a7e2b0f632aaed1a8aee90d49b5fac6925e13
jepzekiel <jezedevkiel21@gmail.com>
2022-05-27 11:17:32 +0800

    Lates Commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Jep's Portfolio website [React Project]
+
+// {
+//   "hosting": {
+//     "public": "build",
+//     "ignore": [
+//       "firebase.json",
+//       "**/.*",
+//       "**/node_modules/**"
+//     ],
+//     "rewrites": [
+//       {
+//         "source": "**",
+//         "destination": "/index.html"
+//       }
+//     ]
+//   }
+// }
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,8 +1,9 @@
 const firebaseConfig = {
-   apiKey: "AIzaSyAu2pY8U1olKZVMjWuC0chv2JZ0IiV14Ao",
-   authDomain: "jepfolio.firebaseapp.com",
-   projectId: "jepfolio",
-   storageBucket: "jepfolio.appspot.com",
-   messagingSenderId: "1049779629450",
-   appId: "1:1049779629450:web:8eb778e60d4900acb86c73"
- };
+  apiKey: "AIzaSyAnQbZh0fbp2V92N-Hs2Ku8bBHnbQxjWZM",
+  authDomain: "jepfolio-web-app.firebaseapp.com",
+  projectId: "jepfolio-web-app",
+  storageBucket: "jepfolio-web-app.appspot.com",
+  messagingSenderId: "560807565784",
+  appId: "1:560807565784:web:8bb0a556aeb741693cbaad",
+  measurementId: "G-2PFSRDWBF4"
+};
