8decbc7a012b09fcaeda4ff58b83a1356a309a4f
jepzekiel <jezedevkiel21@gmail.com>
2022-05-27 10:23:37 +0800

    Initail Commit

---
+++ firebaseConfig.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyAu2pY8U1olKZVMjWuC0chv2JZ0IiV14Ao",
+   authDomain: "jepfolio.firebaseapp.com",
+   projectId: "jepfolio",
+   storageBucket: "jepfolio.appspot.com",
+   messagingSenderId: "1049779629450",
+   appId: "1:1049779629450:web:8eb778e60d4900acb86c73"
+ };
